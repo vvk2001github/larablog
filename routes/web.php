dc8c66d62c61deab6ca98c64d388a9141e7f8f47
kulyaginv <kulyaginv@gmail.com>
2022-05-05 11:40:30 +0300

    Welcome Controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Welcome;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,9 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [Welcome::class, 'index']);
 
 Route::middleware([
     'auth:sanctum',
