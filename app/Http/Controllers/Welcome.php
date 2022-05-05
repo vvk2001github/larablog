cd3c868b4d575d3c3af931d47ebdf03e60c9d3a9
kulyaginv <kulyaginv@gmail.com>
2022-05-05 16:01:47 +0300

    Welcome Page

--- app/Http/Controllers/Welcome.php
+++ app/Http/Controllers/Welcome.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class Welcome extends Controller
 {
     public function index()
     {
-        return view('welcome');
+        $articles = DB::table('articles')
+            ->where('published', 1)
+            ->orderBy('created_at', 'desc')
+            ->paginate(4);
+
+        return view('welcome', compact('articles'));
     }
 }
