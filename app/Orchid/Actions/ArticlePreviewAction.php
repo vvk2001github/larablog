5c6449473d923e3d4531ec5f20e03eb5d86e5bdf
kulyaginv <kulyaginv@gmail.com>
2022-05-06 16:37:35 +0300

    2022-05-06 1613

---
+++ app/Orchid/Actions/ArticlePreviewAction.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Orchid\Actions;
+
+use Illuminate\Support\Collection;
+use Orchid\Crud\Action;
+use Orchid\Screen\Actions\Button;
+use Orchid\Support\Facades\Toast;
+
+class ArticlePreviewAction extends Action
+{
+    /**
+     * The button of the action.
+     *
+     * @return Button
+     */
+    public function button(): Button
+    {
+        return Button::make('Run Action')->icon('fire');
+    }
+
+    /**
+     * Perform the action on the given models.
+     *
+     * @param \Illuminate\Support\Collection $models
+     */
+    public function handle(Collection $models)
+    {
+        $models->each(function () {
+            // action
+        });
+
+        Toast::message('It worked!');
+    }
+
+    public static function editBreadcrumbsMessage(): string
+    {
+        return "Редактировать статью";
+    }
+}
